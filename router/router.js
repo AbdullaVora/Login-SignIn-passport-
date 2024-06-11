85d917e356febe880493b8df8f148e188e224627
Abdulla Vora <143266743+AbdullaVora@users.noreply.github.com>
2024-06-11 10:41:42 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -13,7 +13,7 @@ const {
   logOut,
   loginPage,
 } = require("../controllers/controller");
-const { isAuth, authentication } = require("../middleware/middleware");
+const { isAuth} = require("../middleware/middleware");
 const passport = require("passport");
 
 const router = Router();
@@ -30,7 +30,7 @@ router.post("/sign", addUser);
 router.get("/signPage", signPage);
 router.get("/logOut",logOut);
 router.get("/formPage", formPage);
-router.post("/addBlog", Upload, authentication, addBlog);
+router.post("/addBlog", Upload, addBlog);
 router.get("/delete", deleteBlog);
 router.get("/editPage", editPage);
 router.post("/editData", Upload, update);
